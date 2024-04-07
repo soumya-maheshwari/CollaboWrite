d1950bd74c2c20d0d255e10f8fe42d2c99c8311d
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-04-07 19:45:12 +0530

    ADD: socket.io connection

--- server.js
+++ server.js
@@ -1,7 +1,72 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
+
+const io = new Server(server);
+
+const userSocketMap = {};
+
+function getAllConnectedClients(roomID) {
+  return Array.from(io.sockets.adapter.rooms.get(roomID) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[username],
+      };
+    }
+  );
+}
+
+io.on("connection", (socket) => {
+  console.log(socket, "socket");
+  socket.on("join", ({ roomID, username }) => {
+    console.log(roomID, "roomID");
+    console.log(username, "username");
+
+    userSocketMap[socket.id] = username;
+    socket.join(roomID);
+
+    const clients = getAllConnectedClients(roomID);
+
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on("change_code", ({ roomId, code }) => {
+    socket.in(roomId).emit("change_code", {
+      code,
+    });
+  });
+
+  socket.on("sync_code", ({ socketId, code }) => {
+    console.log(code);
+    io.to(socketId).emit("change_code", {
+      code,
+    });
+  });
+
+  socket.on("disconnecting", () => {
+    console.log(socket.rooms);
+    const rooms = [...socket.rooms];
+    console.log(rooms);
+    rooms.forEach((roomId) => {
+      console.log(roomId);
+      socket.to(roomId).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
+});
 
 const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => console.log(`server listening at ${PORT}`));
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import codeImg from "../assets/code.svg";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [uuid, setuuid] = useState("");
+  const [username, setUsername] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!uuid) {
+      document.getElementById("uuid").style.display = "block";
+    } else {
+      document.getElementById("uuid").style.display = "none";
+    }
+    if (!username) {
+      document.getElementById("username").style.display = "block";
+    } else {
+      document.getElementById("username").style.display = "none";
+    }
+  }, [uuid, username]);
+
+  const submit = () => {
+    if (!uuid || !username) {
+      return;
+    } else {
+      navigate(`/editor/${uuid}`);
+    }
+  };
   return (
     <>
       <div className="home-container">
@@ -9,15 +35,31 @@ const Home = () => {
           <img src={codeImg} alt="</>" className="code-img" />
         </div>
         <div className="form-container">
-          <form action="">
+          <form action="" onSubmit={submit}>
             <div className="form-group">
               <label htmlFor="room_id">Room ID</label>
-              <input type="text" className="input-field" />
+              <input
+                type="text"
+                className="input-field"
+                value={uuid}
+                onChange={(e) => setuuid(e.target.value)}
+              />
+              <p className="error" id="uuid">
+                Room ID is required
+              </p>
             </div>
 
             <div className="form-group">
               <label htmlFor="room_id">Username</label>
-              <input type="text" className="input-field" />
+              <input
+                type="text"
+                className="input-field"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <p className="error" id="username">
+                Username is required
+              </p>
             </div>
 
             <button className="submit-btn btn">Submit</button>
