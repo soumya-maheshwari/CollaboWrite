9c52c74f2f6bfa152794d84b53a0ff02728aef38
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2024-04-07 15:08:38 +0530

    ADD: code editor features

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -7,9 +7,29 @@ import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
 
 const Editor = () => {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    async function init() {
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("code-area"),
+        {
+          mode: {
+            name: "javascript",
+            json: true,
+          },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        }
+      );
+    }
+    init();
+  }, []);
   return (
     <>
-      <textarea id="code-area"></textarea>;
+      <textarea id="code-area"></textarea>
     </>
   );
 };
